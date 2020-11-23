1c8bbb2f19d50baeb9dc633ff60f075872ac23c6
Jed <olivier.begue.begue@gmail.com>
2020-11-23 15:06:27 +0100

    Gestion des fichiers

---
+++ app/src/main/java/com/example/tdrecyclerview/tasklist/TaskListFragment.kt
@@ -0,0 +1,13 @@
+package com.example.tdrecyclerview.tasklist
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.tdrecyclerview.R
+
+class TaskListFragment : Fragment()
+{
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //val rootView = inflater.inflate(R.layout.fragment_task_list, container, false)
+    }
+}
