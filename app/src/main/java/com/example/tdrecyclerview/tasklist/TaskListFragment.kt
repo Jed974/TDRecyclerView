a947e884688b3938fadef7f395f26303ff1d52c5
Jed <olivier.begue.begue@gmail.com>
2020-11-23 15:45:47 +0100

    TaskListFragment

--- app/src/main/java/com/example/tdrecyclerview/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/example/tdrecyclerview/tasklist/TaskListFragment.kt
@@ -1,13 +1,18 @@
 package com.example.tdrecyclerview.tasklist
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.tdrecyclerview.R
 
 class TaskListFragment : Fragment()
 {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        //val rootView = inflater.inflate(R.layout.fragment_task_list, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View?
+    {
+        //return super.onCreateView(inflater, container, savedInstanceState)
+        val rootView = inflater.inflate(R.layout.fragment_task_list, container, false)
+        return rootView
     }
 }
