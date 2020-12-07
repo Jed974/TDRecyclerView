397f08878c9fe5adb721766bb6dc0cfa085c75b0
Jed <olivier.begue.begue@gmail.com>
2020-12-07 15:31:47 +0100

    Network package

---
+++ app/src/main/java/com/example/tdrecyclerview/network/UserInfo.kt
@@ -0,0 +1,11 @@
+package com.example.tdrecyclerview.network
+
+/*@Serializable
+data class UserInfo(
+    @SerialName("email")
+    val email: String,
+    @SerialName("firstname")
+    val firstName: String,
+    @SerialName("lastname")
+    val lastName: String
+)*/
