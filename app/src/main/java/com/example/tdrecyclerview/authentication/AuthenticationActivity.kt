851caee193fa259b191704d903a5236808550550
Jed <olivier.begue.begue@gmail.com>
2020-12-30 17:08:06 +0100

    TD7, new fragments and layout

---
+++ app/src/main/java/com/example/tdrecyclerview/authentication/AuthenticationActivity.kt
@@ -0,0 +1,14 @@
+package com.example.tdrecyclerview.authentication
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.tdrecyclerview.R
+
+class AuthenticationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+
+
+    }
+}
